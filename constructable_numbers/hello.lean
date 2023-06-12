a97bdf71133bf7bafb5d5572052593c6c917bae1
Chris Liu <chris.cl587@gmail.com>
2023-06-12 14:30:48 -0700

    made changes to ...

--- constructable_numbers/hello.lean
+++ constructable_numbers/hello.lean
@@ -1,1 +1,21 @@
+import Mathlib.Algebra.CubicDiscriminant
+import Mathlib.FieldTheory.Adjoin
+
 def hello := "world"
+
+def xcubed_eq_2 : Cubic ℚ where
+  a := 3
+  b := 0
+  c := 0
+  d := 2
+
+
+noncomputable def roots := Cubic.roots xcubed_eq_2
+
+def my_field := IntermediateField.adjoin ℚ roots
+
+noncomputable def xcubed_eq_2_poly := Cubic.toPoly xcubed_eq_2 
+
+
+def q_field : Field ℚ := sorry
+
