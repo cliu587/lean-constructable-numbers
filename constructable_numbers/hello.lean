1130778f8e3486c203679ef6041b506054ecb009
Chris Liu <chris.cl587@gmail.com>
2023-06-14 07:15:58 -0700

    A bit more working

--- constructable_numbers/hello.lean
+++ constructable_numbers/hello.lean
@@ -1,5 +1,4 @@
 import Mathlib.FieldTheory.IntermediateField
--- import Mathlib.Analysis.SpecialFunctions.Pow.Real
 import Mathlib.Algebra.CubicDiscriminant
 import Mathlib.Analysis.Complex.Polynomial
 import Mathlib.FieldTheory.IsAlgClosed.Basic
@@ -7,9 +6,11 @@ import Mathlib.RingTheory.RootsOfUnity.Basic
 import Mathlib.Data.Nat.Parity
 import Mathlib.Data.Real.Irrational
 
+-- https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/!4.234085
+local macro_rules | `($x ^ $y)   => `(HPow.hPow $x $y)
+
 noncomputable section
 
--- open scoped Classical Polynomial
 open Polynomial
 
 variable {F : Type} {E : Type}
@@ -40,22 +41,15 @@ lemma induction (P : constructable → Prop)
 (rad : ∀ a : constructable, P (a^2) → P a)
 (a : constructable) : P a := sorry
 
-def cbrt_two: ℝ := Real.rpow 2 (1/3)
-def cbrt_two_cubed_eq_2 : (cbrt_two)^3 = 2 := by 
-  have two_nonneg: 0 ≤ (2: ℝ) := by norm_num
-  rw[cbrt_two]; norm_num
-  rw [←Real.rpow_mul two_nonneg]; norm_num
-
-def cbrt_two_irrational: Irrational cbrt_two := by
-  apply irrational_nrt_of_notint_nrt 3 2
-  · 
-    -- Issue with power rewriting, TODO
-    sorry
-  · rintro ⟨y, pf⟩
-    -- cbrt_two has decimal, shouldn't be too impossible.
-    sorry
-  · norm_num
-
+def cbrt_two: ℝ := Real.rpow (2: ℝ) (3⁻¹: ℝ)
+def cbrt_two_cubed_eq_2 : (cbrt_two)^(3: ℕ) = 2 := by 
+  have two_nn: 0 ≤ (2: ℝ) := by norm_num
+  have three_nz: (3: ℕ) ≠ (0) := by norm_num
+  have cbrt_cubed_rw: ((2:ℝ) ^ ((3:ℝ)⁻¹)) ^ (3: ℕ) = 2 := by
+    have tmp := Real.rpow_nat_inv_pow_nat two_nn three_nz
+    dsimp at tmp
+    exact tmp
+  rw[cbrt_two];dsimp; rw[cbrt_cubed_rw]
 
 def p: ℚ[X] := X^3 - C 2
 
@@ -67,60 +61,75 @@ def monic_p : Monic p := by
 def p_nonzero: p ≠ 0 := by 
   apply Monic.ne_zero monic_p
 
--- Only TODO is the rewriting business.
 def cbrt_two_evals_to_zero: eval₂ (algebraMap ℚ ℝ) (cbrt_two) (p) = 0 := by 
   have pow_cubed := eval₂_X_pow (n:=3) (R:= ℚ) (S:= ℝ) (algebraMap ℚ ℝ)
-  have pow_cubed_cbrt_two := pow_cubed cbrt_two
-  -- TODO: something problematic with power maps, but below should work after fixing type issues
-  -- rw[cbrt_two_cubed_eq_2] at pow_cubed_cbrt_two
-  -- TODO: This should be by substitution as well
-  have x_cubed_cbrt_two_eq_two : eval₂ (algebraMap ℚ ℝ) cbrt_two (X ^ 3) = 2 := by sorry
-
+  have x_cubed_cbrt_two_eq_two : eval₂ (algebraMap ℚ ℝ) cbrt_two (X ^ 3) = 2:= by
+    have := pow_cubed cbrt_two
+    rw[cbrt_two_cubed_eq_2] at this
+    exact this
+    
   have two_c : eval₂ (algebraMap ℚ ℝ) cbrt_two (C 2) = 2 := by apply eval₂_C
 
   have x_cubed_minus_two_eq_zero: eval₂ (algebraMap ℚ ℝ) cbrt_two (X ^ 3 - C 2) = 0 := by 
-    have tmp := eval₂_sub (algebraMap ℚ ℝ) (x:= cbrt_two) (R := ℚ) (S := ℝ) (p := X^3) (q := C 2)
-    rw[x_cubed_cbrt_two_eq_two, two_c, sub_self 2] at tmp
-    exact tmp
+    have := eval₂_sub (algebraMap ℚ ℝ) (x:= cbrt_two) (R := ℚ) (S := ℝ) (p := X^3) (q := C 2)
+    rw[x_cubed_cbrt_two_eq_two, two_c, sub_self 2] at this; exact this
 
   apply x_cubed_minus_two_eq_zero
 
--- TODO: No root means no linear factor...
-lemma irreducible_of_not_root (f : ℚ[X]) (hfdeg : f.degree ≤ 3) (hf : ∀ x, f.eval x ≠ 0) :
-  Irreducible f := sorry
+-- TODO
+def irreducible_p: Irreducible p := by sorry
 
--- Not sure how to prove yet...
-def irreducible_p : Irreducible p := by 
+
+-- Gauss' Lemma doesn't seem ported: cannot reduce to Z irreducibility.
+def irreducible_p_eisenstein: Irreducible p := by
+  -- have p_is_eisenstein : IsEisensteinAt p (2) (R := ℤ):= sorry
+  sorry
+
+-- Perhaps promising but not sure how to prove
+-- Uses the fact that cbrt_two is irrational so eval x (X^3-2) ≠ 0
+-- for any rational x
+def irreducible_p_not_linear_factor: Irreducible p := by 
+
+  have cbrt_two_irrational: Irrational cbrt_two := by
+    apply irrational_nrt_of_notint_nrt 3 2
+    · rw[cbrt_two_cubed_eq_2]; rfl
+    · rintro ⟨y, pf⟩
+      -- Prove false from cbrt_two = (y: ℤ)
+      -- cbrt_two has decimal, shouldn't be too impossible.
+      sorry
+    · norm_num
+
+  -- No root of a degree ≤ 3 polynomial means no linear factor. 
+  -- Probably will use Polynomial.Monic.irreducible_iff_natDegree' ?
+  have irreducible_of_not_root (f : ℚ[X]) (hfdeg : f.degree ≤ 3) (hf : ∀ x, ¬(f.eval x = 0)) : Irreducible f := sorry
+
   have p_leq_3 : p.degree ≤ 3 := by
-    have p_deg_three : p.degree = 3 := by apply degree_X_pow_sub_C (by norm_num) (a := 2)
+    have p_deg_three : p.degree = 3 := degree_X_pow_sub_C (by norm_num) (a := 2)
     exact le_of_eq p_deg_three
 
   -- Unclear how to prove this.
-  have eval_nnz : ∀ x, p.eval x ≠ 0 := by
+  have eval_nnz : ∀ x, ¬(p.eval x = 0) := by
     intro x
-    intro pxz 
-    rw[p] at pxz
-    apply Irrational.ne_rat cbrt_two_irrational x
-    sorry
+    by_contra pxz
 
+    rw[p] at pxz
+    have is_root : IsRoot p x := pxz
+    have pxz' : eval x ((X^3): ℚ[X]) = 2 := by 
+      -- apply eval_add (p:=pxz) (q := C (2:ℚ)) (x := x)
+      sorry
+    let cbrt_two_roots := nthRoots 3 (2: ℚ)
+    have zero_le_three: 0 < 3 := by norm_num
+    have x_in_cbrt_two_roots : x ∈ cbrt_two_roots := by
+      apply (mem_nthRoots (zero_le_three)).mpr
+      rw[←pxz']
+      simp
+    
+    have cbrt_two_ne_x := Irrational.ne_rat cbrt_two_irrational x (x := cbrt_two)
+
+    sorry
   apply irreducible_of_not_root p p_leq_3 eval_nnz
 
-  -- Direct proof would be below.
-  -- apply irreducible_iff.mpr
-  -- constructor
-  -- · 
-  --   intro p_unit
-  --   rw[p] at p_unit
-  --   have x_cubed_deg_zero := natDegree_eq_zero_of_isUnit p_unit
-  --   have x_cubed_deg_three := natDegree_X_pow_sub_C (n:=3) (r := (2: ℚ))
-  --   rw[x_cubed_deg_three] at x_cubed_deg_zero
-  --   contradiction
-  -- · 
-  --   intro p1 p2 h
-  --   sorry
-
 def p_is_min_poly: p = minpoly ℚ cbrt_two := by apply minpoly.eq_of_irreducible_of_monic irreducible_p cbrt_two_evals_to_zero monic_p
-
 
 def cbrt_two_is_integral : IsIntegral ℚ cbrt_two := by
   refine Iff.mp isAlgebraic_iff_isIntegral ?_
@@ -133,38 +142,33 @@ def cbrt_two_is_integral : IsIntegral ℚ cbrt_two := by
       simp
 
 
--- Proposition to be proved inductively.
+-- Type of proposition constructable numbers satisfies that cbrt_two does not.
 def rank_pow_two_over_ℚ (a : constructable) : Prop := ∃(n: ℕ), FiniteDimensional.finrank ℚ ℚ⟮a.val⟯ = 2^n
 
--- If a number is constructable it sits in a field extension of rank 2^n (to be proven)
+-- If a number is constructable it sits in a field extension of rank 2ⁿ (to be proven)
 lemma constructable_implies_rank_pow_two_over_ℚ (a: constructable) : rank_pow_two_over_ℚ a := sorry
 
-lemma cbrt_two_not_constructable: ¬is_constructable_ℝ cbrt_two := by
-  intro h
+-- Main theorem
+theorem cbrt_two_not_constructable: ¬is_constructable_ℝ cbrt_two := by
+  by_contra h
   let c : constructable := ⟨_, h⟩
-  -- (2^{1/3})^3 = 2, use later to justify p is monic
-
-  -- (2^{1/3})^3 = 2 -> [ℚ⟮2^{1/3}⟯: ℚ] = 3
+  -- [ℚ⟮cbrt_two⟯: ℚ] = 3
   have ℚ_adj_cbrt_two_rank_eq_3 : FiniteDimensional.finrank ℚ ℚ⟮cbrt_two⟯ = 3 := by 
-    rw[←p_is_deg_three]
-    rw[p_is_min_poly]
+    rw[←p_is_deg_three, p_is_min_poly]
     exact IntermediateField.adjoin.finrank cbrt_two_is_integral 
-
 
   have ℚ_adj_cbrt_two_rank_eq_two_pow : rank_pow_two_over_ℚ c := constructable_implies_rank_pow_two_over_ℚ c
 
-  have ⟨n, pf_field_ext⟩ := ℚ_adj_cbrt_two_rank_eq_two_pow
-  rw[pf_field_ext] at ℚ_adj_cbrt_two_rank_eq_3
+  -- [ℚ(cbrt_two) : ℚ] = 2^n along with proof 
+  have ⟨(n: ℕ), pf_rank_pow_2_ext⟩ := ℚ_adj_cbrt_two_rank_eq_two_pow
+  rw[pf_rank_pow_2_ext] at ℚ_adj_cbrt_two_rank_eq_3
 
   have even_two_n : Even (2^n) := by
-    apply Nat.even_pow.mpr
-    constructor
-    · simp
-    · 
-      rw[Ne]
-      intro nez
+    apply Nat.even_pow.mpr; constructor
+    · exact Nat.even_iff.mpr rfl
+    · by_contra nez
       rw[nez] at ℚ_adj_cbrt_two_rank_eq_3
-      simp at ℚ_adj_cbrt_two_rank_eq_3
+      contradiction
 
   have even_three : Even (3) := by 
     rw[← ℚ_adj_cbrt_two_rank_eq_3]
